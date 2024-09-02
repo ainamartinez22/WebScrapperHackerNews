a8e57b69f9e15eee91908f5ead65d637d19d268f
Aina Martínez <95122692+ainamartinez22@users.noreply.github.com>
2024-09-02 14:37:27 +0200

    Save results into a file (#7)
    * Add filter static methods
    
    * Add sort static methods
    
    * Add get clean filter sort data method
    
    * Add documentation
    
    * Add load_data_into_file method
    
    * Add documentation

--- project/scrapper_web/scrapper_web.py
+++ project/scrapper_web/scrapper_web.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import datetime
+import json
 
 class ScrapperWeb():
     """A class used to represent a general Web Scrapper
@@ -16,8 +17,10 @@ class ScrapperWeb():
     Methods
     -------
     get_request(this)
-        Makes a get request to an URL and returns de html parsed by BeautifulSoup. It saves the time of the request.
-
+        Makes a get request to an URL and returns the html parsed by BeautifulSoup. It saves the time of the request.
+    load_data_into_file(data:dict,file_path_name:str)
+        Loads the data you pass as an argument into a file with the file name and path you enter
+    
     """
     def __init__(this,url: str,current_time = None):
         this.url = url
@@ -25,9 +28,23 @@ class ScrapperWeb():
     
 
     def get_request(this):
+        """Makes a get request to an URL and returns the html parsed by BeautifulSoup. 
+            It saves the time of the request in this.current_time"""
+
         this.current_time = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
         page = requests.get(this.url)
         # Parse the HTML content
         soup = BeautifulSoup(page.text, 'html.parser')
         return soup
     
+    def load_data_into_file(this,data:dict,file_path_name:str):
+        """Load data into a file
+
+        Args:
+            data (dict): data to save
+            file_path_name (str): file path you want to save the data (it creates the file)
+        """
+        
+        with open(file_path_name, 'w', encoding='utf-8') as file:
+            json.dump(data, file, ensure_ascii=False, indent=4)
+
