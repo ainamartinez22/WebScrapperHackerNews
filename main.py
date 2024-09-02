3edaf66df37f4ddaf7b725181291765061525f98
Aina Martínez <ainamartinez22@gmail.com>
2024-09-02 13:46:38 +0200

    Add HackerNewsScrapper class and main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+from project.hacker_news_scrapper.hacker_news_scrapper import HackerNewsScrapper
+
+def main():
+    
+    HackerNewsScrapper().run_scrapper()
+
+if __name__ == '__main__':
+    main()
